424bd3c92d8bd145db9f1853950a76caa5dfcb52
q-0z <36612185+q-0z@users.noreply.github.com>
2022-04-21 08:54:38 +0530

    Environment Changes

---
+++ Assets/Script/EnvManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]private List<GameObject> _envObj;
+    [SerializeField] private int _envPoolCount=10;
+    [SerializeField]private Transform _envStart;
+
+    private Queue<GameObject> _envPool= new Queue<GameObject>();
+    void Start()
+    {
+        for(int i=0;i<_envPoolCount;i++)
+        {
+            var Obj = Instantiate(_envObj[0],transform);
+            Obj.SetActive(false);
+            _envPool.Enqueue(Obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
