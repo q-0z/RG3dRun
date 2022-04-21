55249cf0049583822a773c5c328a9d38767c3d24
q-0z <36612185+q-0z@users.noreply.github.com>
2022-04-21 08:31:41 +0530

    FirstPush

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Vector3 velocity;
+    public Animator _animator;
+
+    float side = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // velocity.z = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forward = Input.GetAxisRaw("Vertical");
+        float right = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -3)
+        {
+            side += -3;
+            StartCoroutine(SLerp(side));
+            _animator.SetInteger("right", (int)right);
+
+            // velocity.x =- 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && transform.position.x < 3)
+        {
+            side += 3;
+            StartCoroutine(SLerp(side));
+            _animator.SetInteger("right", (int)right);
+
+
+            //velocity.x = 3;
+        }
+        else
+        {
+            _animator.SetInteger("right", (int)right);
+            // side = 0;
+
+            // velocity.x = 0;
+        }
+
+        // velocity.x = Mathf.Lerp(velocity.x, side, Time.deltaTime * 12);
+        // velocity.x = Mathf.Lerp(velocity.x, side, Time.deltaTime * 12);
+
+        transform.position = new Vector3(velocity.x, velocity.y, transform.position.z+1);
+    }
+
+
+    IEnumerator SLerp(float end)
+    {
+        {
+            while (velocity.x > end)
+            {
+                velocity.x -= .5f;
+                yield return null;
+                // await System.Threading.Tasks.Task.Delay(1);
+                // yield return new WaitForEndOfFrame();
+               // yield return new WaitForSeconds(Time.deltaTime / 10000);
+
+            }
+            while (velocity.x < end)
+            {
+                velocity.x += .5f;
+                yield return null;
+
+                //  yield return new WaitForEndOfFrame();
+
+                // await System.Threading.Tasks.Task.Delay(1);
+               // yield return new WaitForSeconds(Time.deltaTime / 10000);
+
+            }
+            velocity.x = end;
+
+        }
+        
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody _rigidBody;
+    Animator _animator;
+
+    float _animBlendRatio = 0.5f;
+    float _runSpeed = 10;
+    public float updatedX = 0;
+    Vector3 velocity;
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        velocity = transform.position;
+    }
+
+    void Update()
+    {
+        //  float forward = Input.GetAxisRaw("Vertical") > _animBlendRatio || Input.GetAxisRaw("Vertical") < -_animBlendRatio ? Input.GetAxisRaw("Vertical") : 0;
+        // float right = Input.GetAxisRaw("Horizontal") > _animBlendRatio || Input.GetAxisRaw("Horizontal") < -_animBlendRatio ? Input.GetAxisRaw("Horizontal") : 0;
+
+        // _animator.SetFloat("vertical", Input.GetAxis("Vertical"));
+        // _animator.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+
+        //_animator.
+
+        float forward = Input.GetAxisRaw("Vertical");
+        float right = Input.GetAxisRaw("Horizontal");
+        _animator.SetInteger("forward", (int)forward);
+
+
+        //velocity=transform.position+ transform.forward;
+
+       // if(transform.position.x < 4 && transform.position.x>-4)
+        {
+
+            if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -3)
+            {
+                 velocity.x += -3;
+                // updatedX=velocity.x + -3;
+                Debug.LogError(transform.position);
+               // transform.position = new Vector3(transform.position.x - 3, transform.position.y, transform.position.z);
+                _animator.SetInteger("right", (int)right);
+            }
+            else if (Input.GetKeyDown(KeyCode.D) && transform.position.x < 3)
+            {
+                velocity.x += 3;
+
+                // updatedX = velocity.x + 3;
+            //    transform.position = new Vector3(transform.position.x + 3, transform.position.y, transform.position.z);
+
+                _animator.SetInteger("right", (int)right);
+
+            }
+            else
+            {
+                velocity.x = 0;
+                _animator.SetInteger("right", (int)right);
+            }
+        }
+
+        //if(velocity.x<=updatedX)
+        //  velocity.x  =Mathf.Lerp(updatedX,velocity.x  , Time.deltaTime * 10);
+        velocity.z = 1;
+        transform.position += velocity * Time.deltaTime * _runSpeed;
+
+        //_rigidBody.centerOfMass = Vector3.zero;
+        //_rigidBody.velocity = Vector3.zero;
+
+
+
+        //  Vector3 velocity = (forward * transform.forward + right * transform.right).normalized * Time.deltaTime * _currentSpeed;
+
+    }
+
+    IEnumerator Movement()
+    {
+        
+        yield return new WaitForEndOfFrame();
+    }
+
+}
